26787e08c3b417b2bacbaf0cba5658e5acc268fa
Jason Brewer <rockmyifone@gmail.com>
2015-04-07 18:25:26 -0600

    Pretest

---
+++ pretest/pretest.js
@@ -0,0 +1,19 @@
+var firstName = ("Jason");
+	
+var lastName = ("Brewer");
+
+
+console.log((lastName) ,  (firstName));
+
+
+for(var i = 1; i < 16; i++) {
+if (i == 2 || i== 4  || i==6 || i==8 || i== 10 || i==12 || i==14) {
+	console.log( i + "*");
+}
+
+
+else {
+console.log(i);
+
+}
+}
